84a88d6e74cce93e276fa75e598043423c5bda03
Davy FHM <n00218969@upn.pe>
2023-10-04 15:44:05 -0500

    all

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+const btnNav = document.getElementById("btn-nav");
+btnNav.addEventListener("click", ()=>{
+    const iopen = document.getElementById("i-open");
+    const iclose = document.getElementById("i-close");
+    iopen.classList.toggle("d-none");
+    iclose.classList.toggle("d-none");
+})
