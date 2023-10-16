850975f28d28f7884a1d5c86e84cf08cf7a0e14d
Davy FHM <n00218969@upn.pe>
2023-10-15 20:50:59 -0500

    anclas

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-
 const btnNav = document.getElementById("btn-nav");
-btnNav.addEventListener("click", ()=>{
+btnNav.addEventListener("click", () => {
     const iopen = document.getElementById("i-open");
     const iclose = document.getElementById("i-close");
     iopen.classList.toggle("d-none");
     iclose.classList.toggle("d-none");
 })
+
+
+
+
+window.addEventListener('load', cierraVentana);
+
+function cierraVentana() {
+    if (window.innerWidth <= 991) {
+        var listaItems = document.querySelectorAll(".navbar-collapse li");
+        for (var i = 0; i < listaItems.length; i++) {
+            listaItems[i].addEventListener("click", () => {
+                btnNav.click();
+            });
+        }
+    }
+}
 
