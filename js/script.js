144c8a9b0c02d63570db06806a86244ec5918ce3
Davy FHM <n00218969@upn.pe>
2023-11-13 04:22:04 -0500

    share links

--- js/script.js
+++ js/script.js
@@ -61,3 +61,10 @@ animarContador('cont4',63,3000);
 
 //::::::::SAHRE
 
+document.getElementById('whatsapp-button').addEventListener('click', function() {
+  // Obtén el número de teléfono del atributo "data-number"
+  var phoneNumber = this.getAttribute('data-number');
+
+  // Abre el enlace de WhatsApp con el número de teléfono
+  window.open('https://api.whatsapp.com/send?phone=' + phoneNumber + '&text=Hola,%20quisiera%20ayudar!%20%F0%9F%98%8A', '_blank');
+});
